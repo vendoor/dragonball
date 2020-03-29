60bda816a0b8e269b2e4f152a76fbc0b95bb1f39
Attila Bagossy <bagossyattila@outlook.com>
2020-03-29 14:45:44 +0200

    Setup will only succeed if the database does not exist

--- src/main/kotlin/me/vendoor/dragonball/api/setup/SetupDslProcessor.kt
+++ src/main/kotlin/me/vendoor/dragonball/api/setup/SetupDslProcessor.kt
@@ -11,13 +11,19 @@ import me.vendoor.dragonball.api.dsl.upsert.CreateDatabaseContext
 import me.vendoor.dragonball.api.dsl.upsert.CreateIndexContext
 import me.vendoor.dragonball.api.migration.MigrationPerformer
 import me.vendoor.dragonball.api.util.database.hasCollection
+import me.vendoor.dragonball.api.util.database.hasDatabase
 import org.bson.BsonDocument
 import org.bson.conversions.Bson
 import java.lang.Exception
 
 class DatabaseSpecificationProcessor(val configuration: Configuration, val client: MongoClient,
-                                             val context: CreateDatabaseContext) {
+                                     val context: CreateDatabaseContext) {
     fun process() {
+        if (client.hasDatabase(configuration.database.name)) {
+            println("The database already exists! Please drop it first!")
+            return
+        }
+
         val database = client.getDatabase(configuration.database.name)
 
         context.collections.forEach { collection ->
