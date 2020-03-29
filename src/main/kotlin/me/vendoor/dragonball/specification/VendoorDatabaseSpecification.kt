6d89c163a8fa91d51fdc8f0b5d7eea3e7f91f5e1
Attila Bagossy <bagossyattila@outlook.com>
2020-03-29 15:18:52 +0200

    Removed the name call from the spec

--- src/main/kotlin/me/vendoor/dragonball/specification/VendoorDatabaseSpecification.kt
+++ src/main/kotlin/me/vendoor/dragonball/specification/VendoorDatabaseSpecification.kt
@@ -3,8 +3,6 @@ package me.vendoor.dragonball.specification
 import me.vendoor.dragonball.api.dsl.upsert.database
 
 fun vendoorDatabaseSpecification() = database {
-    name { "vendoor" }
-
     version { "1.0.0" }
 
     collections {
